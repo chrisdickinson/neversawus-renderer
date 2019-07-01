0b36bd907c9b6a7c14cf546d1dd8fbf38e85bad4
Chris Dickinson <christopher.s.dickinson@gmail.com>
2019-06-30 20:32:43 -0700

    fix: only render markdown files please.

--- create-zip.js
+++ create-zip.js
@@ -9,6 +9,6 @@ const fs = require('fs')
 const archive = archiver('zip', {
   zlib: { level: 9 }
 })
-archive.pipe(fs.createWriteStream('renderer.zip'))
+archive.pipe(fs.createWriteStream('dist/renderer.zip'))
 archive.file('dist/main.js', {name: 'lib/index.js'})
 archive.finalize()
